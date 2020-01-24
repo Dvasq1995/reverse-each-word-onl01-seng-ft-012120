f51febff504946433d1cc673a72bc2f2fd2e3955
Diego Vasquez <botellongo69@outlook.com>
2020-01-24 20:08:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
   newArr = str.split()
   counter = 0
-  newArr.collect do |i|
+  newArr.collect do
     while counter < newArr.size 
       newArr[counter] = newArr[counter].reverse
       counter += 1
